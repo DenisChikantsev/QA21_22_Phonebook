8cd355a9f8fd2b6cfa46fdb83e61ef1ceca778d6
Denis <svet.puhovski1991@gmail.com>
2023-12-13 22:13:33 +0300

    Login Test

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,34 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class HelperUser extends HelperBase{
+public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
         super(wd);
+    }
+
+    public void openLoginRegistrationForm() {
+//        WebElement loginTab = wd.findElement((By.cssSelector("a[href='/login']")));
+        //xpath //a[text()='LOGIN']
+//        loginTab.click();
+        click(By.cssSelector("a[href='/login']"));
+    }
+
+    public void fillLoginRegistrationForm(String email, String password) {
+//        WebElement emailInput = wd.findElement(By.cssSelector("input[1]"));
+        type(By.name("email"), email);
+
+//        WebElement passwordInput = wd.findElement(By.cssSelector("input[2]"));
+        type(By.name("password"), password);
+    }
+
+    public void submitLogin() {
+        click(By.xpath("//button[text()='Login']"));
+    }
+
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//button[text()='Sign Out']"));
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,12 +1,23 @@
 package tests;
 
+import manager.HelperUser;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
+public class LoginTests extends TestBase {
 
     @Test
-    public void test(){
-        
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("teste@toster.com", "TesterToster123$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue();
+//        Assert.assertFalse();
     }
 
 }
